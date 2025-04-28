9c857fecdc28a061270fb8739d327286024a46a5
ManuelGarreton <magarreton@uc.cl>
2025-04-28 02:25:01 -0400

    MALO

--- src/pages/StockDetailsPage.jsx
+++ src/pages/StockDetailsPage.jsx
@@ -1,144 +1,180 @@
-import React, { useState } from 'react';
-import { Button, Container, Row, Col, Table } from 'react-bootstrap';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Pagination from 'react-bootstrap/Pagination';
+import { Button, Row, Col, Form, InputGroup } from 'react-bootstrap';
+import { StockGeneral } from '../components/stock_general';
 
-export const StockDetails = () => { // Stock de ejemplo hardcodeado
-  const stock = {
-    symbol: "AAPL",
-    price: 150.75,
-    longName: "Apple Inc.",
-    quantity: 100,
-    timestamp: new Date().toLocaleString('en-GB', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: false,
-    }).replace(",", ""),
-  };
+function StocksPage() {
+  const [stocks, setStocks] = useState([]); // Almacenamos los stocks obtenidos
+  const [currentPage, setCurrentPage] = useState(1); // Página actual
+  const [totalPages, setTotalPages] = useState(1); // Total de páginas
+  const [filters, setFilters] = useState({
+    quantity: '',
+    price: '',
+    date: ''
+  }); // Filtros
 
-  const [quantity, setQuantity] = useState(1); // Cantidad que el usuario quiere comprar
-  const [purchaseStatus, setPurchaseStatus] = useState(null); // Estado para mostrar el resultado de la compra
-  const [purchaseHistory, setPurchaseHistory] = useState([  // Hardcodeamos el historial de compras
-    {
-      date: "2025-04-20 (15:30:00)",
-      quantity: 10,
-      status: "Compra exitosa",
-    },
-    {
-      date: "2025-04-21 (10:15:45)",
-      quantity: 5,
-      status: "Compra exitosa",
-    },
-  ]); // Array para el historial de compras
-  const [showHistory, setShowHistory] = useState(false); // Estado para controlar la visibilidad del historial
+  useEffect(() => {
+    // Función para obtener los stocks con paginación
+    const fetchStocks = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/stocks/grouped`, {
+          params: {
+            page: currentPage,
+            count: 12,
+            quantity: filters.quantity,
+            price: filters.price,
+            date: filters.date
+          }
+        });
 
-  const handlePurchase = () => {
-    if (quantity <= stock.quantity) {
-      setPurchaseStatus('Compra exitosa');
-      // Simulamos que la compra fue exitosa agregando al historial
-      const newPurchase = {
-        date: new Date().toLocaleString('en-GB', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-          hour12: false,
-        }).replace(",", ""),
-        quantity,
-        status: 'Compra exitosa',
-      };
-      setPurchaseHistory([newPurchase, ...purchaseHistory]); // Agregar la nueva compra al inicio del historial
-      // Esto habria que sacarlo, pq luego el historial es una consulta de las compras que s ehan hecho hacia ese stock.
+        const formattedStocks = response.data.data.map(stock => {
+          // Formateamos la fecha de cada stock
+          const formattedTimestamp = new Date(stock.timestamp).toLocaleString('en-GB', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false,
+          }).replace(",", "");
+
+          return { ...stock, formattedTimestamp };
+        });
+
+        setStocks(formattedStocks);
+
+        const dataTotalPages = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/stocks/grouped`)
+        const totalPages = dataTotalPages.data.totalEntries
+        setTotalPages(Math.ceil(totalPages / 12));
+      } catch (error) {
+        console.error('Error fetching stocks:', error);
+      }
+    };
+
+    fetchStocks(); // Llamamos a la función para obtener los stocks
+
+  }, [currentPage, filters]); // Se ejecuta cuando cambia la página o los filtros
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage); // Cambiamos la página actual si es válida
     }
-    else {
-      setPurchaseStatus('Error: No hay suficiente stock disponible');
-    }
   };
 
-  const toggleHistory = () => {
-    setShowHistory(!showHistory); // Alternar la visibilidad del historial
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters({
+      ...filters,
+      [name]: value
+    });
   };
+
+  // Dividimos los stocks en tres columnas
+  const thirdIndex = Math.ceil(stocks.length / 3); // Dividimos los stocks en 3 partes
+  const leftColumnStocks = stocks.slice(0, thirdIndex); // Stocks de la columna izquierda
+  const centerColumnStocks = stocks.slice(thirdIndex, thirdIndex * 2); // Stocks de la columna central
+  const rightColumnStocks = stocks.slice(thirdIndex * 2); // Stocks de la columna derecha
 
   return (
     <>
-    <div className='contenedor_stock'>
-      <div key={stock.symbol} className="contenedor_detalles_stock">
-        <div className="titulo_detalles_stock">
-          <h2>{stock.symbol}</h2>
-          <h2 id='precio_stock'>${stock.price}</h2>
-        </div>
-        <div className='info_detalles_stock'>
-          <Row>
-            <p><b>Empresa:</b> {stock.longName}</p>
-            <p><b>Disponible:</b> {stock.quantity}</p>
-            <p><b>Última actualización:</b> {stock.timestamp}</p>
-          </Row>
+      <div className='titulo_page'>
+        <h1>Mercado de Stocks</h1>
+      </div>
 
-          <div className='formulario_compra'>
-                <div className="form-group">
-                  <label>Cantidad: </label>
-                  <input
-                    type="number"
-                    value={quantity}
-                    onChange={(e) => setQuantity(Number(e.target.value))}
-                    min="1"
-                    max={stock.quantity} // No puede comprar más de lo disponible
-                    className="form-control_cantidad"
-                  />
-                </div>
-                <Button onClick={handlePurchase} type="submit" variant='detalle' className='sel_cantidad'>Comprar</Button>
+      <div className='filtros'>
+        <Form className="grupo_filtros">
+          <div>
+            <Form.Label>Stocks disponibles: </Form.Label>
+            <Form.Control
+              type="number"
+              name="quantity"
+              value={filters.quantity}
+              onChange={handleFilterChange}
+              placeholder="Cantidad"
+            />
           </div>
-          {/* {purchaseStatus && <div>{purchaseStatus}</div>} {// Muestra el estado de la comprad} d*/}
-          {/* Botón para mostrar u ocultar el historial */}
+          <div>
+            <Form.Label>Precio: </Form.Label>
+            <InputGroup.Text>$</InputGroup.Text>
+            <Form.Control
+              type="number"
+              name="price"
+              value={filters.price}
+              onChange={handleFilterChange}
+              placeholder="$"
+            />
+          </div>
+          <div>
+            <Form.Label>Fecha: </Form.Label>
+            <Form.Control
+              type="date"
+              name="date"
+              value={filters.date}
+              onChange={handleFilterChange}
+              placeholder="Fecha"
+            />
+          </div>
+          <Button
+            type="button"
+            variant="opcion"
+            id="boton_filtrar"
+            onClick={() => setCurrentPage(1)} // Resetear a la primera página
+          >
+            Filtrar
+          </Button>
+        </Form>
+      </div>
+
+      <div className="page-container">
+        <div className="left-column">
+          {leftColumnStocks.map((stock) => (
+            <div key={stock.symbol} className="stock-item">
+              <StockGeneral {...stock} />
+            </div>
+          ))}
+        </div>
+
+        <div className="center-column">
+          {centerColumnStocks.map((stock) => (
+            <div key={stock.symbol} className="stock-item">
+              <StockGeneral {...stock} />
+            </div>
+          ))}
+        </div>
+
+        <div className="right-column">
+          {rightColumnStocks.map((stock) => (
+            <div key={stock.symbol} className="stock-item">
+              <StockGeneral {...stock} />
+            </div>
+          ))}
         </div>
       </div>
 
-      </div>
-      <div className='contenedor_stock'>
-        <div className='historial_compra'>
-            <Button onClick={toggleHistory} variant="detalle" style={{ marginTop: '20px' }}>
-              {showHistory ? "Ocultar historial" : "Ver historial"}
-            </Button>
-
-            {/* Mostrar el historial solo si el estado showHistory es true */}
-            {showHistory && (
-              <div>
-                <h3>Historial de Compras</h3>
-                <Table className='tabla_historial'>
-                    <thead>
-                        <tr>
-                            <th>Fecha</th>
-                            <th>Cantidad</th>
-                            <th>Status</th>
-                            <th>Stock</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {purchaseHistory.map((purchase, index) => (
-                      <tr>
-                        <td>{purchase.date}</td>
-                        <td>{purchase.quantity}</td>
-                        <td>{purchase.status}</td>
-                        <td>{stock.symbol}</td>
-                      </tr>
-                    ))} 
-                    </tbody>
-                </Table>
-                
-                  
-                
-              </div>
-            )}
-          </div>
+      {/* Paginación */}
+      <div className='paginacion'>
+        <Pagination className='paginacion'>
+          <Col className='col_paginacion'>
+            <Pagination.First onClick={() => handlePageChange(1)} disabled={currentPage === 1} />
+          </Col>
+          <Col className='col_paginacion'>
+            <Pagination.Prev onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} />
+          </Col>
+          <Col className='col_paginacion'>
+            <Pagination.Item className='actual_paginacion'>{currentPage}</Pagination.Item>
+          </Col>
+          <Col className='col_paginacion'>
+            <Pagination.Next onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} />
+          </Col>
+          <Col className='col_paginacion'>
+            <Pagination.Last onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} />
+          </Col>
+        </Pagination>
       </div>
     </>
-    
-    
   );
-};
+}
 
-export default StockDetails;
+export default StocksPage;
